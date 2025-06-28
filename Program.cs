d0fab867384d6930f99e3cdab7b23ff98f4a24dd
Parastoo Ghanbarzade <parastoo.ghanbarzade@gmail.com>
2025-06-28 09:01:54 -0700

    Switch expression used.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Please enter a number of day :");
+                int NumberOfDay = int.Parse(Console.ReadLine());
+                string NameOfDay = NumberOfDay switch
+                {
+                    1 => "Shanebe",
+                    2 => "Yekshanbe",
+                    3 => "Doshanbe",
+                    4 => "Seshanbe",
+                    5 => "Charshanbe",
+                    6 => "Panjshanbe",
+                    7 => "Jome",
+                    _ => "Unknown Day"
+
+                };
+                Console.WriteLine(NameOfDay);
+            }
+              
         }
     }
 }
